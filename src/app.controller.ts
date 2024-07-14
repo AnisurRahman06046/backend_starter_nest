356469fbb767f507d40528dd0754ef20d948a782
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-07-14 22:14:33 +0600

    updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ApiBadRequestResponse,
+  ApiExcludeEndpoint,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiServiceUnavailableResponse,
+} from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Check API status' })
+  @ApiOkResponse({ description: 'The service is operating correctly' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+  @ApiBadRequestResponse({ description: 'Communication error with the server' })
+  @ApiServiceUnavailableResponse({
+    description: 'The service is not available',
+  })
+  @HttpCode(HttpStatus.OK)
+  @ApiExcludeEndpoint()
+  @Get()
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { HelpersModule } from './app/helpers/helpers.module';
+import { AuthModule } from './app/modules/auth/auth.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ExceptionFilter } from './app/filters';
+import { ResponseInterceptor } from './app/interceptors/response.interceptor';
+
+const MODULES = [
+  CacheModule.register({ isGlobal: true }),
+  DatabaseModule,
+  HelpersModule,
+  AuthModule,
+];
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [...MODULES],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
